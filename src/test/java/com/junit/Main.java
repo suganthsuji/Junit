f669e6676bc657ffe8229f0aeab497a8dd3f799f
suganthsuji <suganthpv.007@gmail.com>
2022-07-28 18:13:01 +0530

    Added: Password Validation

--- src/test/java/com/junit/Main.java
+++ src/test/java/com/junit/Main.java
@@ -7,14 +7,16 @@ import java.util.regex.Pattern;
 
 public class Main {
 
+    //Creating Object Validation
         Validation validation=new Validation();
+    //Junit Test for Name
         @Test
         public void givenfNameString_WhenValidated_ReturnBoolean(){
 
             Boolean result =validation.nameValidation("Suganth");
             Assert.assertEquals(true,result);
     }
-
+    //Junit Test for Name
     @Test
     public void givenlNameString_WhenValidated_ReturnBoolean(){
 
@@ -22,6 +24,7 @@ public class Main {
             Assert.assertEquals(true,result);
 
     }
+    //Junit Test for Email
     @Test
     public void givenEmailString_WhenValidated_ReturnBoolean(){
 
@@ -29,10 +32,19 @@ public class Main {
         Assert.assertEquals(true,result);
 
     }
+    //Junit Test for Number
     @Test
     public void givenNumberString_WhenValidated_ReturnBoolean(){
 
         Boolean result=validation.numberValidation("8072948266");
+        Assert.assertEquals(true,result);
+
+    }
+    //Junit Test for Password
+    @Test
+    public void givenPasswordString_WhenValidated_ReturnBoolean(){
+
+        Boolean result=validation.passwordValidation("Abcd@123");
         Assert.assertEquals(true,result);
 
     }
--- src/test/java/com/junit/Validation.java
+++ src/test/java/com/junit/Validation.java
@@ -3,16 +3,29 @@ package com.junit;
 import java.util.regex.Pattern;
 
 public class Validation {
+
+    //Validation Regex for Name
     public boolean nameValidation(String name){
         String nameRegex="^[A-Z][a-z]{3,}$";
         return Pattern.matches(nameRegex,name);
     }
+
+    //Validation Regex for Email
     public boolean emailValidation(String email){
         String emailRegex="^[a-z0-9]{3,}([.][a-z0-9]{1,})?[@][a-z]{3,}[.][a-z]{2,3}([.][a-z]{2})?$";
         return Pattern.matches(emailRegex,email);
     }
+
+    //Validation Regex for Number
     public boolean numberValidation(String number){
         String numberRegex="^([+][0-9]{2})?[\\s][1-9][0-9]{9}$";
         return Pattern.matches(numberRegex,number);
     }
+
+    //Validation Regex for Password
+    public boolean passwordValidation(String password){
+        String passwordRegex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,10}$";
+        return Pattern.matches(passwordRegex,password);
+    }
+
 }
